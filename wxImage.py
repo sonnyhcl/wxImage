8746b021d09af8a486bbc9df3d2eee11f69fc97f
sonnyhcl <sonnyhcl@163.com>
2017-08-13 13:36:22 +0800

    update: 兼容py2/py3以及增加jupyter版本

--- wxImage.py
+++ wxImage.py
@@ -1,69 +1,55 @@
-import itchat
+
+# coding: utf-8
+
+# In[1]:
+
+
 import os
-
-import PIL.Image as Image
-from os import listdir
 import math
+import itchat
+import PIL.Image as Image
 
-itchat.auto_login(enableCmdQR=True)
+itchat.auto_login(hotReload=True)
 
-friends = itchat.get_friends(update=True)[0:]
+friends = itchat.get_friends(update=True)
+username = friends[0]["UserName"]
+if not os.path.exists(username):
+    os.mkdir(username)
+    for i, friend in enumerate(friends):
+        with open(username + "/" + str(i) + ".png", 'wb') as fileImage:
+            fileImage.write(itchat.get_head_img(userName=friend["UserName"]))
 
-user = friends[0]["UserName"]
+print("itchat login done...")
 
-print(user)
 
-os.mkdir(user)
+# In[2]:
 
-num = 0
-
-for i in friends:
-	img = itchat.get_head_img(userName=i["UserName"])
-	fileImage = open(user + "/" + str(num) + ".jpg",'wb')
-	fileImage.write(img)
-	fileImage.close()
-	num += 1
-
-pics = listdir(user)
-
+pics = os.listdir(username)
 numPic = len(pics)
-
-print(numPic)
 
 eachsize = int(math.sqrt(float(640 * 640) / numPic))
 
-print(eachsize)
-
 numline = int(640 / eachsize)
-
-toImage = Image.new('RGBA', (640, 640))
-
-
-print(numline)
-
-x = 0
-y = 0
-
-for i in pics:
-	try:
-		#打开图片
-		img = Image.open(user + "/" + i)
-	except IOError:
-		print("Error: 没有找到文件或读取文件失败")
-	else:
-		#缩小图片
-		img = img.resize((eachsize, eachsize), Image.ANTIALIAS)
-		#拼接图片
-		toImage.paste(img, (x * eachsize, y * eachsize))
-		x += 1
-		if x == numline:
-			x = 0
-			y += 1
+toImage = Image.new('RGB', (int(numline * eachsize), int(numPic / numline * eachsize)))
 
 
-toImage.save(user + ".jpg")
+# In[3]:
+
+for i, pic in enumerate(pics):
+    try:
+        img = Image.open(username + "/" + pic)
+    except IOError:
+        print("IOError: can not read %d.png" % i)
+    else:
+        img = img.resize((eachsize, eachsize), Image.ANTIALIAS)
+        x = int(i % numline)
+        y = int(i / numline)
+        toImage.paste(img, (x * eachsize, y * eachsize))
+
+toImage.save(username + ".png")
 
 
-itchat.send_image(user + ".jpg", 'filehelper')
+# In[4]:
 
+itchat.send_image(username + ".png", 'filehelper')
 
